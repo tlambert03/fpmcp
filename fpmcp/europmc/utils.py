18d0380bda6c8594c67c6c97f6e482766f2dec90
Talley Lambert <talley.lambert@gmail.com>
2025-11-10 12:38:26 -0500

    cleanup

--- fpmcp/europmc/utils.py
+++ fpmcp/europmc/utils.py
@@ -4,16 +4,26 @@ import requests
 
 from fpmcp.europmc.models import Model as SearchResponse
 
+# https://europepmc.org/RestfulWebService
 ROOT: Final = "https://www.ebi.ac.uk/europepmc/webservices/rest"
 
 
 def _search(query: str) -> SearchResponse:
+    # https://europepmc.org/RestfulWebService#!/Europe32PMC32Articles32RESTful32API/search
     response = requests.get(
         f"{ROOT}/search",
         params={"query": query, "format": "json", "resultType": "core"},
     )
     response.raise_for_status()
     return SearchResponse.model_validate_json(response.text)
+
+
+def _fulltext_xml(pmcid: str) -> str | None:
+    # https://europepmc.org/RestfulWebService#!/Europe32PMC32Articles32RESTful32API/fullTextXML
+    xml_response = requests.get(f"{ROOT}/{pmcid}/fullTextXML")
+    if xml_response.status_code == 200:
+        return xml_response.text
+    return None
 
 
 def get_fulltext_from_europmc(pmid: str) -> str | None:
@@ -25,9 +35,6 @@ def get_fulltext_from_europmc(pmid: str) -> str | None:
     # Check if full text available
     article = result[0]
     if article.inEPMC == "Y" and (pmcid := article.pmcid):
-        # Retrieve full-text XML
-        xml_response = requests.get(f"{ROOT}/{pmcid}/fullTextXML")
-        if xml_response.status_code == 200:
-            return xml_response.text
+        return _fulltext_xml(pmcid)
 
     return None
